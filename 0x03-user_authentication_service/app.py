afb063fa4df583b1c474374828c781e30afb0a4f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-12 10:30:42 +0100

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,70 @@
 #!/usr/bin/env python3
+"""Basic Flask app for a minimal WSGI application."""
+from flask import Flask, jsonify, request, abort, redirect, url_for
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'])
+def welcome() -> str:
+    """Index route that returns a JSON payload."""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """Register a new user using AUTH."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({"message": "Missing email or password"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """Create a new session for a user and store it in a cookie."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
+    abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """Destroy the session if the session_id is valid and redirect to index."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('welcome'))
+    abort(403)
+
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """Return the user's profile if the session_id is valid."""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
